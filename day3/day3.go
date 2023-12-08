b9b6216744fa3594af49f0877e6f4f3f6f0c068d
Austin Isakson <austinjisakson@gmail.com>
2023-12-08 15:01:26 +0100

    day 4 part 1 and part 2, no day 3 :(

--- day3/day3.go
+++ day3/day3.go
@@ -1,3 +1,63 @@
-package day3
+package main
 
-func main() {}
+import (
+	"bufio"
+	"os"
+	"unicode"
+)
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	f, err := os.Open("./test")
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	line1 := []rune{}
+	line2 := []rune{}
+	line3 := []rune{}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		// as I scan through lines, I want to check if there are symbols adjacent to numbers
+		// as apposed to the reverse, so if 35 occupies 2:3 and 2:4 I need to check to see
+		// if there is a non-. symbol at 1:2, 1:3, 1:4, 1:5, 2:2, 2:5, 3:2, 3:3, 3:4, or 3:5
+		// So I need to store the line above and below each number into memory, split each
+		// of the spaces into a rune, determine which runes are part of entire numbers
+	}
+	println("Part 1:")
+}
+
+func part2() {
+
+}
+
+func isRuneAdjacent(positions []int, line1 []rune, line2 []rune, line3 []rune) bool {
+	for _, pos := range positions {
+		return isSymbol(line1[pos-1]) ||
+			isSymbol(line1[pos]) ||
+			isSymbol(line1[pos+1]) ||
+			isSymbol(line2[pos-1]) ||
+			isSymbol(line2[pos]) ||
+			isSymbol(line2[pos+1]) ||
+			isSymbol(line3[pos-1]) ||
+			isSymbol(line3[pos]) ||
+			isSymbol(line3[pos+1])
+	}
+	return false
+}
+
+func isSymbol(char rune) bool {
+	if char != '.' && !unicode.IsNumber(char) {
+		return true
+	}
+	return false
+}
--- day4/day4.go
+++ day4/day4.go
@@ -1,3 +1,152 @@
-package day4
+package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// part1()
+	part2()
+}
+
+func part1() {
+	sum := 0
+
+	f, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		cardVal := 0
+		text := scanner.Text()
+		winningNumbers := getWinningNumbers(text)
+		myNumbers := getMyNumbers(text)
+
+		for _, winNumber := range winningNumbers {
+			for j, myNumber := range myNumbers {
+				if winNumber == myNumber {
+					fmt.Println(winNumber)
+					if cardVal == 0 {
+						cardVal = 1
+					} else {
+						cardVal *= 2
+					}
+
+					myNumbers = append(myNumbers[:j], myNumbers[j+1:]...)
+
+					break
+				}
+			}
+		}
+		println(cardVal)
+		sum += cardVal
+	}
+
+	fmt.Println(sum)
+}
+
+func part2() {
+	sum := 0
+
+	f, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	countMap := make(map[int]int)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		cardVal := 0
+		text := scanner.Text()
+		cardNum := getCardNum(text)
+		winningNumbers := getWinningNumbers(text)
+		myNumbers := getMyNumbers(text)
+		for _, winNumber := range winningNumbers {
+			for j, myNumber := range myNumbers {
+				if winNumber == myNumber {
+					cardVal++
+
+					myNumbers = append(myNumbers[:j], myNumbers[j+1:]...)
+
+					break
+				}
+			}
+		}
+
+		copies := countMap[cardNum]
+		if copies == 0 {
+			countMap[cardNum] = 1
+			copies = 1
+		}
+
+		println(cardNum, cardVal, copies)
+
+		for i := 1; i <= cardVal; i++ {
+			if _, exists := countMap[cardNum+i]; exists {
+				countMap[cardNum+i] += copies
+			} else {
+				countMap[cardNum+i] = copies + 1
+			}
+			val := countMap[cardNum+i]
+			println(cardNum+i, val)
+		}
+
+		// Find out how many times I have won on my current card
+		// Find out how many copies of my current card I have won
+		// for each time I have won, add the number of copies of my card to the next card in the sequence
+	}
+
+	for _, val := range countMap {
+		sum += val
+	}
+
+	fmt.Println(sum)
+}
+
+func getCardNum(text string) int {
+	firstSplit := strings.Split(text, ": ")
+	secondSplit := strings.Fields(firstSplit[0])
+	cardNum, _ := strconv.Atoi(secondSplit[1])
+
+	return cardNum
+}
+
+func getWinningNumbers(text string) []int {
+	var winningNumbers []int
+
+	firstSplit := strings.Split(text, ": ")
+	secondSplit := strings.Split(firstSplit[1], " |")
+	winningString := secondSplit[0]
+	winningSplit := strings.Fields(winningString)
+	for _, number := range winningSplit {
+		integer, _ := strconv.Atoi(number)
+		winningNumbers = append(winningNumbers, integer)
+	}
+
+	return winningNumbers
+}
+
+func getMyNumbers(text string) []int {
+	var myNumbers []int
+
+	firstSplit := strings.Split(text, ": ")
+	secondSplit := strings.Split(firstSplit[1], " |")
+	myString := secondSplit[1]
+	mySplit := strings.Fields(myString)
+	for _, number := range mySplit {
+		integer, _ := strconv.Atoi(number)
+		myNumbers = append(myNumbers, integer)
+	}
+
+	return myNumbers
+}
