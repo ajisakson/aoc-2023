5493eec1d05312d6d9429c079d802d83999e66ce
Austin Isakson <austinjisakson@gmail.com>
2023-12-10 13:34:47 +0100

    day 5 setup

--- day5/day5.go
+++ day5/day5.go
@@ -1,3 +1,32 @@
-package day5
+package main
 
-func main() {}
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	sum := 0
+
+	f, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		text := scanner.Text()
+		println(text)
+	}
+
+	println(sum)
+}
+
+func part2() {
+}
